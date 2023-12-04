d25e7afa46246355683d558c8e6f9614cd5e45c7
Bilal17z <bilalbenbouziane1@gmail.com>
2023-12-04 15:55:57 +0100

    commite2

---
+++ script.js
@@ -0,0 +1,29 @@
+let darkMode = false;
+
+function changeDarkMode() {
+  if (darkMode) {
+    darkMode = false;
+    document.documentElement.style.setProperty("--text-color", "black");
+    document.documentElement.style.setProperty("--background-color", "#bba5dc");
+    document.getElementById("dark-light-mode").innerHTML = "Mode sombre";
+  } else {
+    darkMode = true;
+    document.documentElement.style.setProperty("--text-color", "white");
+    document.documentElement.style.setProperty("--background-color", "black");
+    document.getElementById("dark-light-mode").innerHTML = "Mode lumineux";
+  }
+}
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+})
